aaaa7dd7b4c1393e653433695d90e6d297672c11
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-18 12:09:11 +0100

    drop support for ancient pre-7.1 xextproto
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/sis_driver.c
+++ src/sis_driver.c
@@ -72,12 +72,7 @@
 
 #include "globals.h"
 
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
 
 #include <inputstr.h> /* for inputInfo */
 
